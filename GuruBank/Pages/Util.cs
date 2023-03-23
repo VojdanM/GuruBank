1bce24b1e88769c324c2e415bdcdb8debedad34c
VojdanM <martinvojdanoski@gmail.com>
2023-03-23 17:11:43 +0100

    Initial push

---
+++ GuruBank/Pages/Util.cs
@@ -0,0 +1,77 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.DevTools.V108.Browser;
+using OpenQA.Selenium.DevTools.V108.Page;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuruBank.Pages
+{
+    internal class Util
+    {
+        IWebDriver driver;
+       
+
+        public Util(IWebDriver driver)
+        {
+            this.driver = driver;
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://www.demo.guru99.com/v4");
+        }
+       
+        
+        By username = By.Name("uid");
+        By password = By.Name("password");
+        By loginbutton = By.Name("btnLogin");
+        By expecttitle = By.XPath("//marquee[contains(text(),\"Welcome To Manager's Page of Guru99 Bank\")]");
+        By invalid_expect_title = By.XPath("/html/head");
+        
+        public void UserName(String tekst)
+        {
+            driver.FindElement(username).Clear();
+            driver.FindElement(username).SendKeys(tekst);
+            
+        }
+        
+
+        public void Password(String tekst)
+        {
+            
+            driver.FindElement(password).Clear();
+            driver.FindElement(password).SendKeys(tekst);
+            
+        }
+
+        public void Login()
+        {
+            driver.FindElement(loginbutton).Click();
+        }
+
+        public void ExpectTitle()
+        {
+            Assert.AreEqual("Welcome To Manager's Page of Guru99 Bank", driver.FindElement(expecttitle).Text);
+        }
+
+        public void InvalidExpectTitle()
+        {
+            IAlert alert = driver.SwitchTo().Alert();
+            string alertText = alert.Text;
+            Assert.IsTrue(alertText.Contains("User or Password is not valid"));
+            alert.Accept();
+
+           // Assert.AreEqual(alert.Text, "User or Password is not valid");
+        }
+
+       
+    }
+
+
+
+
+}
+
+
---
+++ GuruBank/Tests/TestScript.cs
@@ -0,0 +1,107 @@
+﻿using GuruBank.Pages;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GuruBank.Tests
+{
+    internal class TestScript
+    {
+      
+
+        [Test]
+
+
+        public void ValidUserValidPass()
+        {
+           IWebDriver driver = new FirefoxDriver();
+           Util util = new Util(driver);
+
+                util.UserName("mngr485288");
+                util.Password("dasahaq");
+                util.Login();
+                util.ExpectTitle();
+                driver.Close();
+                driver.Quit();
+
+            
+            
+        }
+        
+
+        [Test]
+
+        public void InvalidUserValidPass()
+        {
+
+            IWebDriver driver = new FirefoxDriver();
+            Util util = new Util(driver);
+
+                util.UserName("test");
+                util.Password("dasahaq");
+                util.Login();
+                util.InvalidExpectTitle();
+                driver.Close();
+                driver.Quit();
+            
+            
+
+        }
+        
+
+        [Test]
+
+        public void ValidUserInvalidPass()
+        {
+
+            IWebDriver driver = new FirefoxDriver();
+            Util util = new Util(driver);
+
+                util.UserName("mngr485288");
+                util.Password("123456");
+                util.Login();
+                util.InvalidExpectTitle();
+                driver.Close();
+                driver.Quit();
+            
+            
+        }
+        
+
+
+        [Test]
+
+        public void InvalidUserInvalidPass()
+        {
+           IWebDriver driver = new FirefoxDriver();
+           Util util = new Util(driver);
+
+                util.UserName("test");
+                util.Password("123456");
+                util.Login();
+                util.InvalidExpectTitle();
+                driver.Close();
+                driver.Quit();
+            
+        }
+        
+
+
+
+
+
+
+
+    }
+        
+}
